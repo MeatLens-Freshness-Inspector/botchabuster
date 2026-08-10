025d11fe3dfad563a02a69d01fce7dfd4e8bde66
Thalanas110 <amdimate43@gmail.com>
2026-08-10 14:34:41 +0800

    refactor: move profiles into users module

--- backend/src/modules/users/index.ts
+++ backend/src/modules/users/index.ts
@@ -5,5 +5,16 @@ export { UserId } from "./domain/UserId";
 export { GetProfile } from "./application/GetProfile";
 export type { UserProfile, UserRepository } from "./domain/ports/UserRepository";
 export { ProfileServiceGateway } from "./infrastructure/ProfileServiceGateway";
+export { ProfileService, profileService } from "./infrastructure/ProfileService";
+export type {
+  AdminCreateUserInput,
+  AdminProfile,
+  AdminUpdateUserInput,
+  AppRole,
+  PrimaryRole,
+  Profile,
+  PrivilegeSummary,
+  UserRole,
+} from "./infrastructure/ProfileService";
 export { GetProfileController } from "./presentation/controllers/GetProfileController";
 export { UserView } from "./presentation/views/UserView";
