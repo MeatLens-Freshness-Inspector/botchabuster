63cee8ed9b9d6adfb7a7556501d83da475594f55
Thalanas110 <amdimate43@gmail.com>
2026-08-10 11:34:26 +0800

    feat: add typed application errors

---
+++ backend/src/shared/domain/errors/ApplicationError.ts
@@ -0,0 +1,30 @@
+export abstract class ApplicationError extends Error {
+  readonly isOperational = true;
+
+  protected constructor(
+    message: string,
+    public readonly statusCode: number,
+  ) {
+    super(message);
+    this.name = new.target.name;
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
+
+export class ValidationError extends ApplicationError {
+  constructor(message = "Invalid request") {
+    super(message, 400);
+  }
+}
+
+export class AuthorizationError extends ApplicationError {
+  constructor(message = "Forbidden") {
+    super(message, 403);
+  }
+}
+
+export class NotFoundError extends ApplicationError {
+  constructor(resource: string) {
+    super(`${resource} not found`, 404);
+  }
+}
