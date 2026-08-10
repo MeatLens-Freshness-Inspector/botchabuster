a1bab4b5f4f37ae2450bcf32dcb865709dd8c2c6
Thalanas110 <amdimate43@gmail.com>
2026-08-10 15:07:04 +0800

    refactor: move developer presentation

--- backend/src/modules/developer/index.ts
+++ backend/src/modules/developer/index.ts
@@ -3,3 +3,7 @@ export {
   DeveloperDashboardStorageService,
   developerDashboardStorageService,
 } from "./infrastructure/DeveloperDashboardStorageService";
+export { default as developerDashboardRoutes } from "./presentation/dashboard-routes";
+export { default as developerOptionsRoutes } from "./presentation/options-routes";
+export { DeveloperDashboardController } from "./presentation/controllers/DeveloperDashboardController";
+export { DeveloperOptionsController } from "./presentation/controllers/DeveloperOptionsController";
